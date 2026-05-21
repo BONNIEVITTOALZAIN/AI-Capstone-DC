1cfe9e931c05384083e2ad77db4a8452e7a2d0a6
evelyn-zhan <evelynzanpx@gmail.com>
2026-05-21 22:32:24 +0700

    update revenue forecasting route

---
+++ api/routes/revenue.py
@@ -0,0 +1,14 @@
+from flask import request, jsonify
+from api.services.revenue import predict
+
+def init_revenue_route(app):
+    @app.route("/predict/revenue", methods=["POST"])
+    def predict_revenue():
+        req = request.get_json()
+        revenues = req.get("revenues")
+
+        result = predict(revenues)
+
+        return jsonify({
+            "prediction": result
+        })
