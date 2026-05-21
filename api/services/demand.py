84ab7693f4ee2d56a2fec72109427fc4b77e42f8
evelyn-zhan <evelynzanpx@gmail.com>
2026-05-21 22:46:33 +0700

    update demand forecasting service

---
+++ api/services/demand.py
@@ -0,0 +1,35 @@
+import numpy as np
+from tensorflow.keras.layers import TFSMLayer
+import joblib
+
+model = TFSMLayer("api/models/demand/", call_endpoint="serving_default")
+scaler = joblib.load("api/models/scaler/demand_scaler.pkl")
+
+WINDOW_SIZE=3
+
+def predict(demands, stock):
+    total_demand = 0
+    lasting_day = 0
+
+    demands = np.array(demands).reshape(-1, 1)
+    scaled_demands = scaler.fit_transform(demands)
+
+    while total_demand <= stock:
+        input = scaled_demands.reshape(1, WINDOW_SIZE, 1)
+
+        scaled_prediction = model(input)
+        scaled_prediction = list(scaled_prediction.values())[0]
+        
+        prediction = scaler.inverse_transform(scaled_prediction)
+        prediction = int(prediction[0][0])
+
+        total_demand += prediction
+        lasting_day += 1
+
+        demands = np.append(demands, prediction)
+        demands = demands[1:]
+        demands = np.array(demands).reshape(-1, 1)
+
+        scaled_demands = scaler.transform(demands)
+
+    return [lasting_day, total_demand]
